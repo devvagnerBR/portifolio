d5078a41b18c1d80243cb27affe9b2ff79c46595
wagner guimaraes <wagnerluizsg@gmail.com>
2022-09-29 12:29:13 -0300

    starting project

---
+++ devvagner.com/src/App.jsx
@@ -0,0 +1,11 @@
+import styles from '../src/GlobalStyle.module.css'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>DEVVAGNER.COM</h1>
+    </div>
+  );
+}
+
+export default App;
